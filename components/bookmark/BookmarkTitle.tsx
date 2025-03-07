76f94ae3de8ad69a2c39d3297b3711935a9c0c80
dicky-dns <dickydns1@gmail.com>
2025-03-07 15:12:19 +0700

    update bookmark title

--- components/bookmark/BookmarkTitle.tsx
+++ components/bookmark/BookmarkTitle.tsx
@@ -1,47 +1,65 @@
 "use client"
 
-import { useAnimation, useInView, motion } from "framer-motion"
-import { useEffect, useRef } from "react"
+import React, { useEffect, useRef } from "react"
+import { motion, useAnimation, useInView } from "framer-motion"
+import { cn } from "@/lib/utils"
 
 export default function BookmarkTitle() {
   const ref = useRef(null)
-  const isInView = useInView(ref)
-
+  const inView = useInView(ref)
   const ctrls = useAnimation()
 
   useEffect(() => {
-    if (isInView) {
+    if (inView){
       ctrls.start("visible")
     }
-  }, [ctrls, isInView])
+  }, [ctrls, inView])
 
-  const bookmarkTitleAnimation = {
+  const wordAnimation = {
+    hidden: {},
+    visible:{},
+  }
+
+  const charAnimation = {
     hidden: {
       opacity: 0,
-      y: -150,
+      y: `1em`,
     },
     visible: {
       opacity: 1,
-      y: 0,
+      y: `0em`,
       transition: {
-        duration: 1.8,
+        duration: 2.8,
         ease: [0.2, 0.65, 0.3, 0.9],
-      },
-    },
+      }
+    }
   }
 
   return (
-    <motion.div
-      ref={ref}
-      animate={ctrls}
-      initial="hidden"
-      aria-hidden="true"
-      variants={bookmarkTitleAnimation}
-      className=""
-    >
-      <h2 className="">
-        Bookmark
-      </h2>
-    </motion.div>
+    <h2 
+      role="heading" 
+      className="text-left text-[40px] font-bold leading-[0.9em] tracking-tighter sm:text-[45px] md:text-[60px] lg:text-[80px]">
+      {/* {title.split(" ").map((word, i) => ( */}
+        <motion.span
+          ref={ref}
+          aria-hidden="true"
+          initial="hidden"
+          animate={ctrls}
+          variants={wordAnimation}
+          transition={{
+            delayChildren:  1.25,
+            staggerChildren: 0.05,
+          }}
+          className={cn("inline-block whitespace-nowrap")}
+        >
+            <motion.span 
+            aria-hidden="true"
+            variants={charAnimation}
+             className={cn("inline-block")}>
+              Bookmark
+            </motion.span>
+          {/* ))} */}
+        </motion.span>
+    </h2>
   )
 }
